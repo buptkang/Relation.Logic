cd44761036c5174240cc306926455ef290634a01
buptkang <buptkang@gmail.com>
2015-08-20 10:59:23 -0400

    Initial Commit toward gitHub.

---
+++ Geometry.Shapes/Circle.Cache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpLogic;
+
+namespace AlgebraGeometry
+{
+    public partial class CircleSymbol : ShapeSymbol
+    {
+        public override void UndoGoal(EqGoal goal, object parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnifyProperty(string label, out object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
